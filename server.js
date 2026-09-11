dd6eda2603bb2bd9f5b61501e0014dbd3d2fa828
Joy2502072 <joypradhan@gmail.com>
2026-09-12 02:08:51 +0530

    Update server port for deployment

--- server.js
+++ server.js
@@ -583,11 +583,9 @@ app.get("/api/reports/:id", (req, res) => {
 // START SERVER
 // ==========================================
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    () => {
+app.listen(PORT, "0.0.0.0", () => {
 
         console.log(
             `RoadSense Backend running on http://localhost:${PORT}`
